79891d2f9104ed40c9a578de5bdf21341de7fa0a
Kenny Stanley <kpstanleyjr@gmail.com>
2021-07-06 10:43:44 -0500

    Compartmentalized the run Web Server process

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
-import { createServer } from 'https'
-import { parse } from 'url'
+import { createServer, Server } from 'https'
+import { URL, parse, UrlWithParsedQuery } from 'url'
 import next from 'next'
 import fs from 'fs'
 import path from 'path'
@@ -9,20 +9,48 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-const httpsOptions = {
-    cert: fs.readFileSync(path.join(__dirname, "../../cert/cert.pem")),
-    key: fs.readFileSync(path.join(__dirname, "../../cert/key.pem"))
+// Global Variables
+let webServer: Server
+
+(async () => {
+    try {
+        await runWebServer()
+    } catch (error) {
+        console.log(error)
+    }
+})()
+
+async function runWebServer() {
+    const sslCrt = path.join(__dirname, "../../cert/cert.pem"),
+        sslKey = path.join(__dirname, "../../cert/key.pem")
+
+    if (!fs.existsSync(sslKey) || !fs.existsSync(sslCrt)) {
+        console.error('SSL files are not found. check the README.md for instructions.');
+        process.exit(0);
+    }
+    const tls = {
+        cert: fs.readFileSync(sslCrt),
+        key: fs.readFileSync(sslKey),
+    };
+
+    await new Promise<void>((resolve) => {
+        app.prepare().then(() => resolve())
+    })
+
+    webServer = createServer(tls, (req, res) => {
+        const parsedUrl = parse(req.url!, true);
+        handle(req, res, parsedUrl);
+    });
+
+    webServer.on('error', (err: Error) => {
+        console.error('starting web server failed:', err.message);
+    });
+
+    await new Promise<void>((resolve) => {
+        webServer.listen(port, () => {
+            console.log(`> Server listening at https://localhost:${port} as ${dev ? 'development' : process.env.NODE_ENV
+                }`);
+            resolve();
+        });
+    });
 }
-
-app.prepare().then(() => {
-    createServer(httpsOptions, (req, res) => {
-        const parsedUrl = parse(req.url!, true)
-        handle(req, res, parsedUrl)
-    }).listen(port)
-
-    // tslint:disable-next-line:no-console
-    console.log(
-        `> Server listening at https://localhost:${port} as ${dev ? 'development' : process.env.NODE_ENV
-        }`
-    )
-})
